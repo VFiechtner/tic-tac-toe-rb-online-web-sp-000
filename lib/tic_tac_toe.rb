220faaeddc340ae18331e7142e1ce2af0e083435
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-11 21:51:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ def position_taken?(board, index)
   board[index] == "X" || board[index] == "0"
 end
 
-def move(board, index, token="X")
+def move(board, index, token)
   board[index] = token
 end
 

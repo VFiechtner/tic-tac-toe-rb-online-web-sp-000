7e35d0da4a07d21b971d53732c7e0c268f9053e1
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-11 22:29:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,8 +38,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player)
-
+    token = current_Player(board)
+    move(board, index, token)
   end
 end
 
